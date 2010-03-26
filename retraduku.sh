f18cf7c7402329ac67b40e2118283c05b6851334
Hèctor Alòs i Font <hectoralos@gmail.com>
2010-03-26 22:06:15 +0000

    - Aldonoj de ŝanĝoj de Korin' - Aldono de tekstaro + retraduku.sh - Forigo de portempaj dix

---
+++ retraduku.sh
@@ -0,0 +1,3 @@
+make && cat tekstaro/fr.crp.txt | time apertium fr-eo > nova_traduko.txt
+diff aktuala_traduko.txt nova_traduko.txt | grep -r '[<>]' > /tmp/crpdiff.txt && for i in `cut -c3-8 /tmp/crpdiff.txt | uniq`; do echo  --- $i ---; grep -r "^ *$i\." tekstaro/fr.crp.txt; grep -r " $i\." /tmp/crpdiff.txt; done | less
+
