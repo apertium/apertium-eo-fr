2c8553dd69381310749da3e943cb170f2737b476
Hèctor Alòs i Font <hectoralos@gmail.com>
2010-03-27 20:55:44 +0000

    Prilaboro de vortaroj

--- retraduku.sh
+++ retraduku.sh
@@ -1,4 +1,4 @@
-#make && cat tekstaro/fr.crp.txt | time apertium -d . fr-eo > nova_traduko.txt
-make && cat tekstaro/fr.crp.txt | time apertium -d . fr-eo-bytecode > nova_traduko.txt
+make && cat tekstaro/fr.crp.txt | time apertium -d . fr-eo > nova_traduko.txt
+#make && cat tekstaro/fr.crp.txt | time apertium -d . fr-eo-bytecode > nova_traduko.txt
 diff aktuala_traduko.txt nova_traduko.txt | grep -r '[<>]' > /tmp/crpdiff.txt && for i in `cut -c3-8 /tmp/crpdiff.txt | uniq`; do echo  --- $i ---; grep -r "^ *$i\." tekstaro/fr.crp.txt; grep -r " $i\." /tmp/crpdiff.txt; done | less
 
