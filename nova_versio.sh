68b1bafa15f530cd467a26679a9545f1e095bafd
Hèctor Alòs i Font <hectoralos@gmail.com>
2010-12-02 20:27:45 +0000

    Tractament dels números com det/adj Aprofundiment de l'anàlisi sintàctica Poliments lèxics

--- nova_versio.sh
+++ nova_versio.sh
@@ -3,6 +3,8 @@ cp apertium-eo-fr.fr.dix apertium-eo-fr.fr.dix.$date
 cp apertium-eo-fr.eo.dix apertium-eo-fr.eo.dix.$date
 cp apertium-eo-fr.eo-fr.dix apertium-eo-fr.eo-fr.dix.$date
 cp apertium-eo-fr.fr-eo.t1x apertium-eo-fr.fr-eo.t1x.$date
+cp apertium-eo-fr.fr-eo.antaux_t2x apertium-eo-fr.fr-eo.antaux_t2x.$date
+cp apertium-eo-fr.fr-eo.antaux2_t2x apertium-eo-fr.fr-eo.antaux2_t2x.$date
 cp apertium-eo-fr.fr-eo.t2x apertium-eo-fr.fr-eo.t2x.$date
 cp apertium-eo-fr.fr-eo.t3x apertium-eo-fr.fr-eo.t3x.$date
 cp nova_traduko.txt aktuala_traduko.txt
