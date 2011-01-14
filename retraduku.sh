b1b4ed35632af86299646f3c54cc530486a1b159
Hèctor Alòs i Font <hectoralos@gmail.com>
2011-01-14 22:50:30 +0000

    Millores en regles i dixs

--- retraduku.sh
+++ retraduku.sh
@@ -1,5 +1,5 @@
 #make && cat tekstaro/fr.crp.txt | time apertium -d . fr-eo > nova_traduko.txt
 make && cat tekstaro/fr.crp.txt | time apertium -d . fr-eo-bytecode > nova_traduko.txt
 #make && cat tekstaro/fr.crp.txt | time apertium -d . fr-eo-pn > nova_traduko.txt (tiu modo ne donas bonajn rezultojn)
-diff aktuala_traduko.txt nova_traduko.txt | grep -r '[<>]' > /tmp/crpdiff.txt && for i in `cut -c3-8 /tmp/crpdiff.txt | uniq`; do echo  --- $i ---; grep -r "^ *$i\." tekstaro/fr.crp.txt; grep -r " $i\." /tmp/crpdiff.txt; done | less
+./komparu.sh
 
